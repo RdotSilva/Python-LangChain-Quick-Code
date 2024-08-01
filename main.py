8636e0f19e85f28f8127923b2b737c65f628b3c4
RdotSilva <RyandotSilva@gmail.com>
2024-08-01 21:06:54 +0100

    Add code prompt using template

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = OpenAI()
+code_prompt = PromptTemplate(
+    template="Write a very short {language} function that will {task}",
+    input_variables=["language", "task"],
+)
 
+
+llm = OpenAI()
 
 result = llm("Write a very very short poem")
 
