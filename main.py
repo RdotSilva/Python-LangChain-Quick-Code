5ac4d1a62bd6fcfb9aa33bd80facf4b57eaaba4e
RdotSilva <RyandotSilva@gmail.com>
2024-08-02 18:37:29 +0100

    Add parser for command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import argparse
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
 
 llm = OpenAI()
 
